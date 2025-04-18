4034b2afdc46f4f91d92bfddd82f3afb873ca6b3
TnZzZHlp <tnzzzhlp@outlook.com>
2025-04-18 17:07:59 +0800

    优化哈希值处理逻辑，添加哈希值初始化功能，改进图片转换时的错误处理

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use img2avif::img2avif;
 use indicatif::{ProgressBar, ProgressState, ProgressStyle};
 use rayon::prelude::*;
 use std::io::Write;
+use std::sync::RwLock;
 use std::{
     fmt,
     fs::{File, read_dir},
@@ -30,6 +31,7 @@ struct Args {
 static IMAGE_FORMATS: [&str; 3] = ["jpg", "png", "jpeg"];
 
 static HASHER: OnceLock<Hasher> = OnceLock::new();
+static HASHES: OnceLock<RwLock<Vec<ImageHash>>> = OnceLock::new();
 
 fn main() {
     let args = Args::parse();
@@ -52,15 +54,20 @@ fn main() {
         )
         .unwrap_or_else(|_| panic!("Failed to create hasher"));
 
+    HASHES
+        .set(init_hashes(&args.output_dir))
+        .unwrap_or_else(|_| panic!("Failed to create hashes"));
+
     images.par_iter().for_each(|img_path| {
         let img_path = Path::new(img_path);
 
         // 找到相同的图片
-        match compare_hash(&img_path, &output_dir) {
-            Some(hash) => {
+        match compare_hash(img_path) {
+            Ok(Some(hash)) => {
                 // 转换图片格式
                 let file = File::open(img_path).unwrap();
-                let img = img2avif(file, Some(args.speed), Some(args.quality)).unwrap();
+                let img = img2avif(file, Some(args.speed), Some(args.quality))
+                    .expect("Failed to convert image");
 
                 let output_path = output_dir.join(format!("{}.avif", uuid::Uuid::new_v4()));
                 std::fs::write(output_path, img).unwrap();
@@ -72,15 +79,22 @@ fn main() {
                     .append(true)
                     .open(hash_file_path)
                     .unwrap();
-                writeln!(file, "{}", hash).unwrap();
+                writeln!(file, "{}", hash.to_base64()).unwrap();
+                HASHES.get().unwrap().write().unwrap().push(hash);
                 pb.inc(1);
             }
-            None => {
+            Err(e) => {
+                pb.println(format!("Image {} error: {:?}", img_path.display(), e));
+                pb.inc(1);
+            }
+            Ok(None) => {
                 pb.println(format!("Image {} already exists", img_path.display()));
                 pb.inc(1);
             }
         }
     });
+
+    pb.finish_with_message("Processing complete");
 }
 
 fn find_all_img_recusive<P: AsRef<Path>>(path: P) -> Vec<String> {
@@ -104,27 +118,40 @@ fn find_all_img_recusive<P: AsRef<Path>>(path: P) -> Vec<String> {
 }
 
 // 获取目标文件夹hashes文件内保存的哈希值，然后与传入的Hash值进行对比
-fn compare_hash<P: AsRef<Path>>(img_path: P, output_dir: P) -> Option<String> {
-    let img = image::open(img_path).unwrap();
+fn compare_hash<P: AsRef<Path>>(
+    img_path: P,
+) -> Result<Option<ImageHash>, image::error::ImageError> {
+    let img = image::ImageReader::open(img_path)?
+        .with_guessed_format()?
+        .decode()?;
     let hasher = HASHER.get().unwrap();
     let origin_hash = hasher.hash_image(&img);
 
     // 从文件读取哈希值
-    let hash_file_path = output_dir.as_ref().join("hashes");
+    let hashes = HASHES.get().unwrap().read().unwrap();
 
-    let hashes = std::fs::read_to_string(hash_file_path).unwrap_or_default();
-    let mut hash_vec = Vec::new();
-    for line in hashes.lines() {
-        hash_vec.push(ImageHash::from_base64(line).unwrap());
-    }
-
-    for hash in hash_vec {
+    for hash in hashes.iter() {
         if hash.dist(&origin_hash) < 10 {
-            return None;
+            return Ok(None);
         }
     }
 
-    Some(origin_hash.to_base64())
+    Ok(Some(origin_hash))
+}
+
+// 初始化HASHES
+fn init_hashes(output_dir: &str) -> RwLock<Vec<ImageHash>> {
+    let hashes = RwLock::new(Vec::new());
+    let hash_file_path = Path::new(output_dir).join("hashes");
+    if hash_file_path.exists() {
+        let file = std::fs::read_to_string(hash_file_path).unwrap();
+        for line in file.lines() {
+            if let Ok(hash) = ImageHash::from_base64(line) {
+                hashes.write().unwrap().push(hash);
+            }
+        }
+    }
+    hashes
 }
 
 fn init_pb(len: usize) -> ProgressBar {
